f7ac6b133acfd1bcf9128e8c0d132ed6df62d966
Xiaodong Ming <xiaodong.ming@outlook.com>
2019-02-20 14:04:11 +0000

    improve the display of colorbar, and add function Asc2GeoTiff

--- Visualization.py
+++ Visualization.py
@@ -9,6 +9,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import InputSetupFuncs as IS
 import ArcGridDataProcessing as AP
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+import gzip
+from osgeo import osr, gdal 
+import shutil
 #%% draw dem map with boundary cell scatters
 def DEMwithBoundary(demMat,demHead,
                     boundList=[],
@@ -30,7 +34,12 @@ def DEMwithBoundary(demMat,demHead,
     # draw DEM
     img=plt.imshow(demMat,extent=demExtent)
     # colorbar
-    fig.colorbar(img,ax=ax,fraction=0.1,shrink=0.9)
+	# create an axes on the right side of ax. The width of cax will be 5%
+    # of ax and the padding between cax and ax will be fixed at 0.05 inch.
+    divider = make_axes_locatable(ax)
+    cax = divider.append_axes("right", size="5%", pad=0.05)
+    plt.colorbar(img, cax=cax)
+	
     # draw boundary cells
     cStr='mbkgcmbkgc'
     n=0
@@ -43,10 +52,10 @@ def DEMwithBoundary(demMat,demHead,
     if len(figureName)>0:
         fig.savefig(figureName, dpi=dpi)
     
-    return fig,ax
+    return bnMat
 #%% draw inundation map with domain outline
 def InundationMap(zMat,zHead,depth=0.2,figureName=[],
-                    figsize=(10, 6),dpi=300):
+                    figsize=(10, 6),dpi=300,vmin=0.2,vmax=False):
     # fig,ax = InundationMap(demMat,demHead,boundList,figureName)
     # plot inundation map with domain outline
     # depth: the minimum water depth to show in the map
@@ -62,9 +71,16 @@ def InundationMap(zMat,zHead,depth=0.2,figureName=[],
     # draw inundation
     zMat = zMat+0
     zMat[zMat<depth]=np.nan
-    img=plt.imshow(zMat,extent=zExtent)
+    if vmax!=False:
+        img=plt.imshow(zMat,extent=zExtent,vmin=vmin,vmax=vmax)
+    else:
+        img=plt.imshow(zMat,extent=zExtent)
     # colorbar
-    fig.colorbar(img,ax=ax,fraction=0.1,shrink=0.8)
+	# create an axes on the right side of ax. The width of cax will be 5%
+    # of ax and the padding between cax and ax will be fixed at 0.05 inch.
+    divider = make_axes_locatable(ax)
+    cax = divider.append_axes("right", size="5%", pad=0.05)
+    plt.colorbar(img, cax=cax)
     # draw domain outline cells
     ax.scatter(boundX,boundY,s=0.2,facecolor='r')
     ax.axes.grid(linestyle='-.',linewidth=0.2)
@@ -73,3 +89,74 @@ def InundationMap(zMat,zHead,depth=0.2,figureName=[],
         fig.savefig(figureName, dpi=dpi)
         
     return fig,ax
+
+#%% convert ascii file to geotif
+def Asc2GeoTiff(ascfileName,tiffFileName,srcEPSG=27700,destEPSG=4326):
+    # convert a projected asc file to a tiff with geographical coordinates
+    # srcEPSG = 27700 #BNG
+    # destEPSG = 4326 #WGS84
+    source = ascfileName
+    target = tiffFileName
+    if ~target.endswith('.tif'):
+        target=target+'.tif'
+    # open CSV source file
+    if source.endswith('.gz'):
+        with gzip.open(source, 'rb') as f_in:
+            with open(source[:-3], 'wb') as f_out:
+                shutil.copyfileobj(f_in, f_out)
+        source = source[:-3]
+
+    cvs = gdal.Open(source)
+    #cvs = gdal.Open(source)
+    if cvs is None:
+        print( 'ERROR: Unable to open %s' % source)
+        return
+
+    # get GeoTIFF driver
+    geotiff = gdal.GetDriverByName("GTiff")
+    if geotiff is None:
+        print( 'ERROR: GeoTIFF driver not available.')
+        return None
+
+    # set source coordinate system of coordinates in CSV file
+    src_crs = osr.SpatialReference()
+    src_crs.ImportFromEPSG(srcEPSG)
+
+    # set destination projection parameters
+    dest_crs = osr.SpatialReference()
+    dest_crs.ImportFromEPSG(destEPSG)
+
+    # set coordinate transformation
+    tx = osr.CoordinateTransformation(src_crs, dest_crs)
+
+    # get raster dimension related parameters of source dataset
+    xo, xs, xr, yo, yr, ys = cvs.GetGeoTransform()
+    xsize = cvs.RasterXSize
+    ysize = cvs.RasterYSize
+
+    # convert corner coordinates from old to new coordinate system
+    (ulx, uly, ulz) = tx.TransformPoint(xo, yo)
+    (lrx, lry, lrz) = tx.TransformPoint(xo + xs * xsize + xr * ysize,\
+                                        yo + yr * xsize + ys * ysize)
+
+    # create blank in-memory raster file with same dimension as CSV raster
+    mem = gdal.GetDriverByName('MEM')
+    dest_ds = mem.Create('', xsize, ysize, 1, gdal.GDT_Float32)
+
+    # get new transformation
+    dest_geo = (ulx, (lrx - ulx) / xsize, xr,\
+                uly, yr, (lry - uly) / ysize)
+
+    # set the geotransformation
+    dest_ds.SetGeoTransform(dest_geo)
+    dest_ds.SetProjection(dest_crs.ExportToWkt())
+
+    # project the source raster to destination coordinate system
+    gdal.ReprojectImage(cvs, dest_ds, \
+                        src_crs.ExportToWkt(), dest_crs.ExportToWkt(),\
+                        gdal.GRA_Bilinear, 0.0, 0.0)
+
+    # save projected in-memory raster to disk
+    geotiff.CreateCopy(target, dest_ds, 0 )
+    return None
+
